c3ee0890fcf6972b9d58abc51412dd448579504e
Phyo Min Htun <phyominhtun@Phyos-MacBook-Pro-2.local>
2020-09-21 00:52:30 +0630

    output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "instance_public_ip_addr" {
-  value = aws_instance.this.public_ip
+  value = aws_instance.this.*.public_ip
   description = "Instance Public IP Address"
 }
 
 output "instance_public_dns" {
-  value = aws_instance.this.public_dns
+  value = aws_instance.this.*.public_dns
   description = "Instance Public DNS"
 }
 
